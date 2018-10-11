a482985d6cddb6cc5eeb92c733f922602dcd6e65
Ladybug <auburnete@gmail.com>
2018-10-11 17:54:42 +1000

    Visible hidden

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const make_link = () => {
 	var rand = urls[Math.floor(Math.random() * urls.length)];
-	document.getElementById("message").innerHTML = "";
+	document.getElementById("message").classList.add("invisible");
 	var button = document.getElementById("thebutton");
 	button.href = rand;
 	button.on_click = "";
@@ -8,9 +8,7 @@ const make_link = () => {
 }
 
 const on_click = () => {
-	document.getElementById("message").innerHTML = `
-		<p>Making your sample pack...</p>
-		<img id = "paige" src="images/doc_writing.gif">`;
+	document.getElementById("message").classList.remove("invisible");
 	document.getElementById("thebutton").innerText = "...";
 	setTimeout(make_link, 5000);
 }
