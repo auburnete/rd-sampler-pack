2b773c8b38642fad4bd2c423e136e176acb88c4d
Ladybug <auburnete@gmail.com>
2018-10-11 17:49:08 +1000

    fancy

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const make_link = () => {
 	var rand = urls[Math.floor(Math.random() * urls.length)];
-	document.getElementById("message").innerHTML = `<p><a href=${rand}>Click to download!</a></p>`;
+	document.getElementById("message").innerHTML = "";
+	var button = document.getElementById("thebutton");
+	button.href = rand;
+	button.on_click = "";
+	button.innerHTML = "DOWNLOAD";
 }
 
 const on_click = () => {
-	document.getElementById("message").innerHTML = "<p>Making your sample pack...</p>";
+	document.getElementById("message").innerHTML = `
+		<p>Making your sample pack...</p>
+		<img id = "paige" src="images/doc_writing.gif">`;
+	document.getElementById("thebutton").innerText = "...";
 	setTimeout(make_link, 5000);
 }
